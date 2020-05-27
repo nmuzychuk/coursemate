6582d3f38e6c34dd8cb1490cd56b8689d522d709
Nazarii Muzychuk <muzychuk.n@gmail.com>
2020-05-27 20:22:58 +0300

    Update to jdk11, modularize

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.nmuzychuk.coursemate {
+    requires org.hibernate.orm.core;
+    requires org.apache.commons.lang3;
+    requires java.persistence;
+    requires java.naming;
+
+    opens com.nmuzychuk;
+}
